a759315216623f83aa57167361b8e14f91596263
sonpasavee975 <sunpasavee@gmail.com>
2026-02-16 22:11:18 +0700

    merge-blacklist-review-first-version

---
+++ backend/prisma/migrations/20260216143850_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "ReportStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
+-- CreateEnum
+CREATE TYPE "ReportType" AS ENUM ('INAPPROPRIATE_BEHAVIOR', 'NO_SHOW', 'SCAM', 'SAFETY_CONCERN', 'OTHER');
+
+-- AlterTable
+ALTER TABLE "Route" ADD COLUMN     "actualEndTime" TIMESTAMP(3),
+ADD COLUMN     "actualStartTime" TIMESTAMP(3),
+ADD COLUMN     "optimizeWaypoints" BOOLEAN DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "Report" (
+    "id" TEXT NOT NULL,
+    "reporterId" TEXT NOT NULL,
+    "reportedUserId" TEXT NOT NULL,
+    "penaltyApplied" BOOLEAN NOT NULL DEFAULT false,
+    "bookingId" TEXT,
+    "type" "ReportType" NOT NULL,
+    "description" TEXT NOT NULL,
+    "evidenceUrl" TEXT,
+    "status" "ReportStatus" NOT NULL DEFAULT 'PENDING',
+    "reviewedBy" TEXT,
+    "reviewedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blacklist" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Blacklist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Report_reportedUserId_idx" ON "Report"("reportedUserId");
+
+-- CreateIndex
+CREATE INDEX "Report_status_idx" ON "Report"("status");
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_reporterId_fkey" FOREIGN KEY ("reporterId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_reportedUserId_fkey" FOREIGN KEY ("reportedUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "Booking"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Blacklist" ADD CONSTRAINT "Blacklist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
